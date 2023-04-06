29973658f72484af47529bc66eca30697f8fc8d8
ksj051203 <ksj054703@gmail.com>
2023-04-07 08:24:13 +0900

    FIX :: CrossOrigin

--- src/main/java/com/project/Wayg/Controller/WaygController.java
+++ src/main/java/com/project/Wayg/Controller/WaygController.java
@@ -3,18 +3,20 @@ package com.project.Wayg.Controller;
 import com.project.Wayg.Entity.School;
 import com.project.Wayg.Service.WaygService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@AllArgsConstructor
-@CrossOrigin
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/wayg")
 public class WaygController {
     private final WaygService waygService;
--- src/main/java/com/project/Wayg/WaygApplication.java
+++ src/main/java/com/project/Wayg/WaygApplication.java
@@ -2,18 +2,10 @@ package com.project.Wayg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WaygApplication implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins("*").
-				allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
-				.allowedMethods("*");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaygApplication.class, args);
