bd865686a614c7446e97fecef48984c6dcb827d9
ksj051203 <ksj054703@gmail.com>
2023-04-06 23:42:27 +0900

    FEAT : Cors 설정(WebMVC)

--- src/main/java/com/project/Wayg/Controller/WaygController.java
+++ src/main/java/com/project/Wayg/Controller/WaygController.java
@@ -6,14 +6,19 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("")
 public class WaygController {
     private final WaygService waygService;
+
+    @CrossOrigin(origins = "*", allowCredentials = "false")
     @GetMapping("/school/info")
     public ResponseEntity<List<School>> schoolInfo(){
         List<School> school = waygService.schoolInfo();
--- src/main/java/com/project/Wayg/WaygApplication.java
+++ src/main/java/com/project/Wayg/WaygApplication.java
@@ -2,9 +2,15 @@ package com.project.Wayg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class WaygApplication{
+public class WaygApplication implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**").allowedOrigins("*");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WaygApplication.class, args);
