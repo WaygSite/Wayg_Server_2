38e4ef4315c6a8cbd7af37bbcec2a1a331d45d14
ksj051203 <ksj054703@gmail.com>
2023-04-08 21:05:32 +0900

    CHORE :: change the origins

--- src/main/java/com/project/Wayg/Controller/WaygController.java
+++ src/main/java/com/project/Wayg/Controller/WaygController.java
@@ -4,26 +4,21 @@ import com.project.Wayg.Entity.School;
 import com.project.Wayg.Service.WaygService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/wayg")
 public class WaygController {
     private final WaygService waygService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET})
     @GetMapping("/school/info")
     public List<School> schoolInfo(){
         List<School> school = waygService.schoolInfo();
         return school;
     }
+
 }
--- src/main/java/com/project/Wayg/config/CorsConfiguration.java
+++ src/main/java/com/project/Wayg/config/CorsConfiguration.java
@@ -1,16 +1,19 @@
 package com.project.Wayg.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*");
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
     }
 }
