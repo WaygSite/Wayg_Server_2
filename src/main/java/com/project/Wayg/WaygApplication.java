3213e1a8d440b3e60629dcd456bfc5dc2ef79802
ksj051203 <ksj054703@gmail.com>
2023-05-22 08:51:57 +0900

    DELETE :: Security 기능 제거

--- src/main/java/com/project/Wayg/WaygApplication.java
+++ src/main/java/com/project/Wayg/WaygApplication.java
@@ -2,10 +2,11 @@ package com.project.Wayg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WaygApplication{
 
 	public static void main(String[] args) {
